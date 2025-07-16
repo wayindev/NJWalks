2d309a7d9064fdac9897b1a5415c46cf2d599682
wayindev <85441145+wayindev@users.noreply.github.com>
2025-07-16 23:02:43 +0530

    [Change] : First Lecture Authentication

---
+++ Authentication/NZWalks/NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("krishna")]
+        public IActionResult GetAllStudent()
+        {
+            string[] students = new string[] {"krishna", "Parth", "Babe"};
+            return Ok(students);
+        }
+        
+    }
+}
